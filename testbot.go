9aff827e33f4c83ea257b7d853043d8d51e81390
Javier Celada <celada.jcm.javier@gmail.com>
2018-09-15 17:28:29 +0200

    Groups mock functions

--- testbot.go
+++ testbot.go
@@ -50,6 +50,7 @@ func handleMessageEvent(event *slack.MessageEvent) {
 	} else if checkCommand(event.Text, false) {
 		active=false
 		rtm.SendMessage(rtm.NewOutgoingMessage("Ok, lets have lunch!", event.Channel))
+		organizeGroups(users)
 	} else if isConfirmation(event.Text) {
 		users = addUser(users, event)
 	}
@@ -60,7 +61,7 @@ func checkCommand(message string, isStart bool) bool {
 	if function = stop; isStart  {
 	    function = start
 	}
-	var commandMessage = command + " " + function
+	commandMessage := command + " " + function
 	return message == commandMessage && !active == isStart
 }
 
@@ -75,6 +76,51 @@ func addUser(users []string, event *slack.MessageEvent) []string {
 		rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("<@%s> is in!", event.User), event.Channel))
 	}
 	return users
+}
+
+func organizeGroups(users []string) {
+	users = []string{"a","b","c","d","e","f","g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"}
+	numberOfGroups, groupSize := getNumberOfGruops(len(users))
+	groups:=getGroups(users, numberOfGroups, groupSize)
+	fmt.Printf("END\n")
+	fmt.Printf("Gruops: %v\n", groups)
+}
+
+func getNumberOfGruops(users int) (int, int) {
+	groupSize:= 7
+	groups:=0
+	lastGruopSize:=0
+	fmt.Printf("Users: %v\n", users)
+	for {
+		groups, lastGruopSize = divmod(users, groupSize)
+		if (lastGruopSize > 1) {
+			groups++
+		}
+ 		if (lastGruopSize == 0 || lastGruopSize == groupSize - 1 || lastGruopSize == 1) {
+			break
+ 		}
+ 		groupSize--
+	}
+ 	return groups, groupSize
+}
+
+func getGroups(users []string, numberOfGroups int, groupSize int) [][]string {
+	var groups [][]string
+	for i:=1; i <= numberOfGroups; i++ {
+		start := (i - 1) * groupSize
+		end := (i * groupSize)
+		if (i == numberOfGroups) {
+			end = len(users)
+		}
+		groups = append(groups, users[start:end])
+	}
+	return groups
+}
+
+func divmod(numerator, denominator int) (quotient, remainder int) {
+    quotient = numerator / denominator
+    remainder = numerator % denominator
+    return
 }
 
 func contains(slice []string, search string) bool {
